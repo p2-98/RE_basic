ee783114e0d7d9bd6cd386e2f0b0c72c26993cc7
Phạm Phú Quí <phamphuqui1998@gmail.com>
2016-10-20 15:50:45 +0700

    Create bai4.c

---
+++ task08_nhapmonC_01/BAI_TAP/bai4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int a[100];
+int main()
+{
+    int n, max = 0;
+    scanf("%d", &n);
+    printf("%d",n);
+    for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+            if (a[i] > max)
+                max = a[i];
+        }
+    printf("%d", max);
+    return 0;
+}
+Tìm giá trị lớn nhất trong mảng:
+Input
+3
+1 2 3
+output
+3
