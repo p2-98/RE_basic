ceae1c9e54c713fc938427abe1d37c8bdee32f79
Phạm Phú Quí <phamphuqui1998@gmail.com>
2016-10-22 18:17:10 +0700

    Create Bai6_DTHT.c

---
+++ task08_nhapmonC_02/BAI_TAP/Bai6_DTHT.c
@@ -0,0 +1,16 @@
+#define pi = 3.14
+#include <stdio.h>
+
+int main()
+{
+  float r, s;
+  scanf("%f",&r);
+  s = r * r * pi;
+  printf("%.2f", s);
+}
+/* Nhập vào bán kính hình tròn, hãy tính diện tích của nó
+Input
+2
+Output
+12.56
+*/
