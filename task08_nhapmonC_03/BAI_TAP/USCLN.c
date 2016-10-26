f4177a12f6eeb08bdd27e3a08027432f3fec1563
Phạm Phú Quí <phamphuqui1998@gmail.com>
2016-10-26 21:40:25 +0700

    Create USCLN.c

---
+++ task08_nhapmonC_03/BAI_TAP/USCLN.c
@@ -0,0 +1,27 @@
+#include <stdio.h>;
+#include <math.h>;
+int solve(int m, int n)
+{
+	if (m % n == 0) return n;
+	if (n % m == 0) return m;
+	if (m > n)
+		return solve(m % n, n);
+	else
+	if (m < n)
+		return solve(m, n % m);
+
+}
+int main()
+{
+	int m, n;
+	scanf("%d %d",&m, &n);
+	printf("%d",solve(m, n));
+	return 0;
+}
+
+/*Cho hai số nguyên dương a và b, hãy tìm ước số chung lớn nhất của a và b.
+Dữ liệu nhập:
+- Gồm 2 số a và b cách nhau một khoảng trắng (1 ≤ a, b ≤ 5.000.000)
+Dữ liệu xuất:
+- Là ước số chung lớn nhất của a và b.
+*/
