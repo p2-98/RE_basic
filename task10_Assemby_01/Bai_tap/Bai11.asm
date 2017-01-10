a6d34c53f8a309c157c9e8ca4399279cda77eae0
Phạm Phú Quí <phamphuqui1998@gmail.com>
2017-01-10 08:18:17 -0800

    Add files via upload

---
+++ task10_Assemby_01/Bai_tap/Bai11.asm
@@ -0,0 +1,22 @@
+.MODEL SMALL
+.STACK 100h
+.DATA
+CR EQU 0DH
+LF EQU 0AH
+MSG DB CR,LF,'**********$'
+.CODE
+MAIN PROC
+    mov ax,@data
+    mov ds,ax
+    ;khoi tao lap
+    mov cx,10 
+    print:
+    LEA dx,MSG
+    mov ah,9
+    int 21h
+    loop print
+    ;dos
+    mov ah,4ch
+    int 21h
+MAIN ENDP
+END MAIN
---
+++ task10_Assemby_01/Bai_tap/Bai12.asm
@@ -0,0 +1,51 @@
+.MODEL SMALL
+.STACK 100h
+.DATA
+CR EQU 0DH
+LF EQU 0AH
+MSG DB CR,LF,'***********$'
+msge db cr,lf,'Nhap vao 3 ky tu: $'
+char1 db ?,'$'
+char2 db ?,'$'
+char3 db ?,'$'
+.CODE
+MAIN PROC
+    MOV ax,@data
+    mov ds, ax
+    ;In dau ?
+    MOV ah,2
+    mov dl,'?'
+    int 21h;
+    ;nhap vao 3 ky tu
+    lea dx,msgE
+    mov ah,9
+    int 21h
+    ;1
+    mov ah,1
+    int 21h
+    mov char1, al
+    ;2
+    mov ah,1
+    int 21h
+    mov char2, al
+    ;3
+    mov ah,1
+    int 21h
+    mov char3, al
+    ;
+    ;hien thi bang 11x11 dau sao
+    mov cx,11
+    print:
+    lea dx,msg
+    mov ah,9
+    int 21h
+    loop print
+    ;
+    mov ah,2
+    mov dl,7h
+    int 21h
+    ;dos
+    mov ah,4ch
+    int 21h
+main endp
+end main
