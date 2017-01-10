6d9b3352525f268b980c93604e5851887aff81c9
Phạm Phú Quí <phamphuqui1998@gmail.com>
2017-01-10 07:08:20 -0800

    Add files via upload

---
+++ task10_Assemby_01/Bai_tap/In_Out.asm
@@ -0,0 +1,23 @@
+.MODEL SMALL
+.STACK 100h
+.DATA
+CR EQU 0DH
+LF EQU 0AH
+CHAR DB ?,'$'
+.CODE
+MAIN PROC
+    ;khoi tao DS
+    MOV AX,@DATA
+    MOV DS,AX
+    ;vao mot ky tu
+    MOV AH,1    ;ham doc mot ky tu
+    INT 21h     ;doc 1 chua vao AL 
+    MOV CHAR, AL;luu tru
+    ;hien thi
+    MOV AH,9    ;hien thi
+    INT 21h ;
+    ;DOS
+    MOV AH,4CH
+    INT 21h
+    MAIN ENDP
+    END MAIN
